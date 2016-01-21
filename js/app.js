2e38b7ac63c83591a24d7f1f0499260d09af1fdd
Zachary Smith <billyzacsmith@gmail.com>
2016-01-21 15:26:49 -0700

    Beef up the schema.

--- js/app.js
+++ js/app.js
@@ -8,13 +8,7 @@ app.controller('PostsController', function($scope, $http) {
       console.log('==============success')
       console.log(postsData)
       $scope.posts = postsData.data
-      // this callback will be called asynchronously
-      // when the response is available
     }, function errorCallback(response) {
       console.log('==============error')
-      // console.log(response)
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
     })
-
 })
