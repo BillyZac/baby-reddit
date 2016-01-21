4e5a8cb9bb73fcfcf72029e4699564584791ace0
Zachary Smith <billyzacsmith@gmail.com>
2016-01-21 14:31:47 -0700

    Move fake data into a separate file and make an http request to retrieve the data.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,20 @@
-var app = angular.module('app', []);
-var postsData =
-  [
-    {
-      title: "Title 1",
-      author: "Sam Author",
-      image: "https://www.bforball.com/image/categ/tomato.png"
-    },
-    {
-      title: "Title 1",
-      author: "Sam Author",
-      image: "https://www.bforball.com/image/categ/tomato.png"
-    },
-    {
-      title: "Title 1",
-      author: "Sam Author",
-      image: "https://www.bforball.com/image/categ/tomato.png"
-    }
-  ]
+var app = angular.module('app', ['ngResource']);
 
-app.controller('PostsController', function($scope) {
-  $scope.posts = postsData
+app.controller('PostsController', function($scope, $http) {
+  $http({
+    method: 'GET',
+    url: './data/postsData.json'
+  }).then(function successCallback(postsData) {
+      console.log('==============success')
+      console.log(postsData)
+      $scope.posts = postsData.data
+      // this callback will be called asynchronously
+      // when the response is available
+    }, function errorCallback(response) {
+      console.log('==============error')
+      // console.log(response)
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    })
+
 })
