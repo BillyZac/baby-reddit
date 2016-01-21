9a2211a2eb512d1762ace59fd0d50a23b91b6dea
Zachary Smith <billyzacsmith@gmail.com>
2016-01-21 14:01:46 -0700

    Mock up post list with fake data.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 var app = angular.module('app', []);
+var postsData =
+  [
+    {
+      title: "Title 1",
+      author: "Sam Author",
+      image: "https://www.bforball.com/image/categ/tomato.png"
+    },
+    {
+      title: "Title 1",
+      author: "Sam Author",
+      image: "https://www.bforball.com/image/categ/tomato.png"
+    },
+    {
+      title: "Title 1",
+      author: "Sam Author",
+      image: "https://www.bforball.com/image/categ/tomato.png"
+    }
+  ]
 
 app.controller('PostsController', function($scope) {
-  $scope.posts = [
-    1,
-    2,
-    3
-  ]
+  $scope.posts = postsData
 })
