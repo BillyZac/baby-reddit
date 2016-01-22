0a67fee8f42a7234058e078f54a889b80c74afcb
Zachary Smith <billyzacsmith@gmail.com>
2016-01-21 21:57:51 -0700

    New post form. Takes in data and logs it to the console, but doesn't actually store the new post.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 var app = angular.module('app', []);
 
+app.controller('NewPostController', function($scope) {
+  // set newPost function
+  $scope.newPost = {}
+  $scope.addPost = function () {
+    $scope.newPost.date_created = Date.now()
+    $scope.newPost.votes = 0 // Come back to this. Maybe votes is in the wrong place?
+    $scope.newPost.comments = []
+    console.log('Saving this stuff (hopefully):', $scope.newPost)
+    console.log($scope.posts) // Can I access this from here? Seems not.
+    // Push into the array in the file somehow. Blech.
+    $scope.newPost = {}
+  }
+})
+
 app.controller('PostsController', function($scope, $http) {
   $http({
     method: 'GET',
@@ -10,13 +24,29 @@ app.controller('PostsController', function($scope, $http) {
       $scope.posts = postsData.data
     }, function errorCallback(response) {
       console.log('==============error')
+    }).then(function(){
+
     })
 })
 
-app.controller('NewPostController', function($scope) {
+// Pretty sure I need to move this to the post. A comment is always a child of a post.
+app.controller('NewCommentController', function($scope) {
   $scope.comment = {
     content: 'foo',
     author: 'guy',
     date_created: new Date()
   }
+  // Define a new comment function that pushes a comment onto the posts.comments array.
+})
+
+app.controller('testPost', function($scope) {
+  $scope.favoriteForm = {};
+  $scope.submitFav = function() {
+    var favPi = parseFloat($scope.favoriteForm.favoritePie);
+    // Special output if the favorite pie is a certain number
+    if (!isNaN(favPi) && favPi >= 3.14 && favPi <= 3.142) {
+      $scope.favoriteForm.favoritePie = "\u03A0";
+    }
+    console.log("Your favorite pie is: ", $scope.favoriteForm.favoritePie);
+  };
 })
